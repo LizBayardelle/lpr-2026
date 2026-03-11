f4ac30eb7cc09102546e886c948f45a3a3de4fb8
Ellie Bayardelle <elizabeth@frayatech.com>
2026-03-11 12:54:14 -0700

    FIX: Thumbnails and Alerts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,10 @@ import "./navbar"
 import "./react_mount"
 import "./admin_loans"
 import "./wysiwyg"
+
+// Auto-dismiss flash messages
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-flash]").forEach(el => {
+    setTimeout(() => el.remove(), 4000)
+  })
+})
